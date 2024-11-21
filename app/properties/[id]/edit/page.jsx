65c42e184e389152d7be30d4bf52f6c1bccedeca
ZlataEkert <zlataekert@gmail.com>
2024-11-21 17:44:09 +0200

    Common fixes

--- app/properties/[id]/edit/page.jsx
+++ app/properties/[id]/edit/page.jsx
@@ -19,8 +19,8 @@ const PropertyEditPage = async ({ params }) => {
 
   return (
     <section className="bg-blue-50">
-      <div class="container m-auto max-w-2xl py-24">
-        <div class="container bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="container bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <PropertyEditForm property={property} />
         </div>
       </div>
